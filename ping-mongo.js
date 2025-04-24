449b5cdd1dc601e0e9035e6ab57fed18024a0f39
Sanket Deb <imsanketdeb20@gmail.com>
2025-04-24 12:56:21 +0530

    Initial commit

---
+++ ping-mongo.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+require("dotenv").config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+const pingMongo = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(" Pinged MongoDB ");
+  } catch (err) {
+    console.err(" Ping failed ", err);
+  } finally {
+    await client.close();
+  }
+};
+
+pingMongo();
